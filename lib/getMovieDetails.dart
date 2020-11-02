fbb79d00fbaed0dd71ed42fd2b5d6e7e5c355c63
Baptou <pporte.baptiste@protonmail.com>
2020-11-02 19:36:51 +0100

    Fix the selection of URL

--- lib/getMovieDetails.dart
+++ lib/getMovieDetails.dart
@@ -8,19 +8,17 @@ Future<MovieDetails> fetchMovieDetails(int id, int type) async {
   MovieDetails moviedetails;
   String url;
 
-  if (type == 0) {
-    url = "https://api.themoviedb.org/3/movie/" +
-        id.toString() +
-        "?api_key=62feaff3d2cf094a340f530fbf25bde9";
-  } else if (type == 1) {
-    url = "https://api.themoviedb.org/3/tv/" +
-        id.toString() +
-        "?api_key=62feaff3d2cf094a340f530fbf25bde9";
-  } else if (type == 2) {
-    url = "https://api.themoviedb.org/3/movie/" +
-        id.toString() +
-        "?api_key=62feaff3d2cf094a340f530fbf25bde9";
-  }
+  /*
+  *  Si la cible est un type 1, alors c'est un TV SHOW.
+  *  Sinon, c'est un MOVIE
+  *  Le type 0 est un Film en trending,
+  *  Le type 2 est un film en Best.
+  */
+  String typeString = type == 1 ? "tv" : "movie";
+
+  url = "https://api.themoviedb.org/3/$typeString/" +
+      id.toString() +
+      "?api_key=62feaff3d2cf094a340f530fbf25bde9";
 
   final response = await http.get(url);
 
@@ -32,13 +30,12 @@ Future<MovieDetails> fetchMovieDetails(int id, int type) async {
     moviedetails = MovieDetails.fromJson(jsonResponse);
 
     final responseCast = await http.get(
-        "https://api.themoviedb.org/3/movie/$id/credits?api_key=62feaff3d2cf094a340f530fbf25bde9");
+        "https://api.themoviedb.org/3/$typeString/$id/credits?api_key=62feaff3d2cf094a340f530fbf25bde9");
 
     // var jsonResponse2 = jsonDecode(responseCast.body);
     moviedetails.castingList(jsonDecode(responseCast.body)["cast"]);
     // print(moviedetails);
     return moviedetails;
-    
   } else {
     print("Cannot load page");
     throw Exception('Failed to load details');
