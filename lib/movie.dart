a78763c133bcb2c9df92e19bd0d547a8e5c63a2d
Baptou <pporte.baptiste@protonmail.com>
2020-11-02 16:27:00 +0100

    Added best movies list

--- lib/movie.dart
+++ lib/movie.dart
@@ -18,18 +18,14 @@ class Movie {
   factory Movie.fromJson(Map<String, dynamic> json) {
     print(json);
     return Movie(
-      // title: json["title"] != null ? json["title"] : json["name"],
       overview: json["overview"],
       posterPath: "https://image.tmdb.org/t/p/w500/${json["poster_path"]}",
       id: json["id"],
-      // voteAverage: json["vote_average"],
-      // releaseDate:
-          // json["release_date"] != null ? json["release_date"] : "1967-02-20",
     );
   }
 }
 
-class MovieDetails {
+class MovieDetails extends Movie{
   String title;
   String overview;
   String posterPath;
@@ -42,8 +38,8 @@ class MovieDetails {
     this.overview,
     this.voteAverage,
     this.posterPath,
-    this.tags,
     this.releaseDate,
+    this.tags,
   });
 
   factory MovieDetails.fromJson(Map<String, dynamic> json) {
